bacbb04fd06df49436ead6ae2edb623f5f351603
Ravindra-Mohith <210050112@iitb.ac.in>
2022-08-17 23:34:13 +0530

    Add polynomial rolling hash function

--- hashing.cpp
+++ hashing.cpp
@@ -2,13 +2,22 @@
 #include <string>
 using namespace std;
 
+int pow(int a, int b)
+{
+    int k = 1;
+    for (int i = 0; i < b; i++)
+    {
+        k *= a;
+    }
+    return k;
+}
+
 int hash_string(string s)
 {
     int result = 0;
     for (int i = 0; i < s.length(); i++)
     {
-        result += s[i];
+        result += (s[i] * pow(89, i)) % 13;
     }
     return result % 13;
-    // implement hashing function here
 }
