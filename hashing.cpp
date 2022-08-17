4ba9c266426511154d180384bc1301098323f0f6
Ravindra-Mohith <210050112@iitb.ac.in>
2022-08-17 23:27:19 +0530

    Add hashing header and string read

---
+++ hashing.cpp
@@ -0,0 +1,8 @@
+#include "hashing.hpp"
+#include <string>
+using namespace std;
+
+int hash_string(string s)
+{
+    // implement hashing function here
+}
---
+++ hashing.hpp
@@ -0,0 +1,13 @@
+#ifndef _HASHING
+#define _HASHING
+#include "hashing.cpp"
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int hash_string(string s);
+
+#endif
+
